1340be79fef726cf71ebd86996839878c96f6731
expedition-lab <peeroosuhail047@gmail.com>
2025-10-02 17:30:53 +0400

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,3 +1,6 @@
+// at the very top of EVERY api file
+export const config = { runtime: 'edge', regions: ['iad1', 'fra1', 'dub1'] };
+
 export const config = { runtime: 'edge' };
 
 const json = (obj, status = 200) =>
