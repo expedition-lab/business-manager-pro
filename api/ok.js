903aa6e4a591a81cf3710bf6e554731376d05fac
expedition-lab <peeroosuhail047@gmail.com>
2025-10-02 20:20:55 +0400

    Delete api directory

--- api/ok.js
+++
@@ -1,5 +0,0 @@
-// at the very top of EVERY api file
-export const config = { runtime: 'edge', regions: ['iad1', 'fra1', 'dub1'] };
-
-export const config = { runtime: 'edge' };
-export default function handler() { return new Response('ok', { status: 200 }); }
