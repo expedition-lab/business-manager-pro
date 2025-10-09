78df4c246697844bfe4f9125aef99f2b641d8b43
expedition-lab <peeroosuhail047@gmail.com>
2025-10-09 23:48:15 +0400

    Create server.ts

---
+++ utils/supabase/server.ts
@@ -0,0 +1,16 @@
+import { createServerClient } from "@supabase/ssr";
+import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+
+export function createClient(cookies?: ReadonlyRequestCookies) {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key: string) => cookies?.get(key)?.value,
+        set: () => {},
+        remove: () => {},
+      },
+    }
+  );
+}
